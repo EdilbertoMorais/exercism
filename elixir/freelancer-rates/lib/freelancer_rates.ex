0c8bda40acb6054c384887628c4aa31a3dca7e72
Edilberto Morais <edilbertocmorais@gmail.com>
2022-07-13 17:54:34 -0300

    exercism finalizado freelancer_rates

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,21 @@
 defmodule FreelancerRates do
-  def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
-  end
+  def daily_rate(hourly_rate), do: hourly_rate * 8.0
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    discount_value = before_discount * (discount / 100)
+    before_discount - discount_value
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    monthly_value = daily_rate(hourly_rate) * 22
+
+    apply_discount(monthly_value, discount)
+    |> Float.ceil()
+    |> trunc()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    (budget / apply_discount(daily_rate(hourly_rate), discount))
+    |> Float.floor(1)
   end
 end
